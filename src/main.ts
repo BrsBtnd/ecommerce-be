a08b5f8cab252b910fabb06a09e743a654111020
Botond Boros <boros_botond@yahoo.com>
2023-11-09 12:13:19 +0200

    revert rename push test changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// after rename
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT || 3001);
